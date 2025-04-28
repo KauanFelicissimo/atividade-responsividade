9cee12a516c62fd4bcad8d7454385ea7fd7ccce2
Will4264 <willian.prescinato@gmail.com>
2025-04-28 11:58:12 -0300

    Adicionado o serviço de hospedagem Render

--- hosting-free.php
+++ hosting-free.php
@@ -5,9 +5,29 @@
 
 <main>
 
-    <h3> Hospedagem Gratuita </h3>
+ <div class="tudo">   
+ <h3> Hospedagem Gratuita </h3>
+  <div class="introducao">
+    <h1 class="titulo-render">Render:</h1>
+    <p>O Render é um serviço de hospedagem em nuvem que permite a criação, implantação e escalabilidade de aplicativos web, especialmente aqueles construídos com Docker. É uma plataforma que facilita a vida do desenvolvedor, permitindo que ele foque na lógica do aplicativo, e não na infraestrutura. O Render ajuda você a hospedar aplicativos da web escritos utilizando sua linguagem de programação favorita: Node.js com Express, Python com Django, você escolhe. O Render compila e implementa seu código a cada envio para sua ramificação Git vinculada.</p>   
+  </div>
+
+  <div class="meio">
+    <h2>Plano Gratuito</h2>
+    <p>O Render oferece um plano gratuito para desenvolvimento e hospedagem de aplicações e sites estáticos, com algumas limitações. O plano gratuito é destinado a aplicações e sites que não precisam de muita atividade, pois o serviço pode ser desligado após um período de inatividade. Detalhes do plano gratuito:</p>  
+    <ul class="lista">
+        <li><h2>Aplicações:</h1>O plano gratuito permite a implantação de aplicações web e sites estáticos, mas a performance pode ser comprometida se o serviço estiver inativo há algum tempo.</li>
+        <li><h2>Banco de Dados:</h1>O plano gratuito de banco de dados é eliminado após algumas semanas e pode ser necessário recriar um novo banco de dados e reconectá-lo.</li>
+        <li><h2>Limitações:</h1>O plano gratuito tem limitações em relação ao tempo de inatividade e à velocidade de resposta após o serviço ser reativado, podendo levar até 50 segundos para a resposta.</li>
+        <li><h2>Certificados TLS:</h1>O Render oferece certificados TLS gratuitos para sites e serviços, facilitando a configuração de segurança.</li>
+        <li><h2>Implantação:</h1>A plataforma facilita a implantação de aplicações, permitindo a conexão com repositórios do GitHub ou GitLab, ou a inserção da URL de repositórios públicos.</li>
+        <li><h2>Limites de uso mensal:</h1>O Render concede 750 horas de instância gratuitas para cada espaço de trabalho por mês.</li>        
+    </ul>
+  </div>
+  </div>  
 
 </main>
+
 
 
 <?php include 'partial/footer.php'; ?>
